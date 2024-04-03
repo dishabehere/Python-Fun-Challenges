4e4257b963779c9b94c50d30c2f07f8a22029ea3
Disha Behere <123135306+dishabehere@users.noreply.github.com>
2024-04-03 14:31:29 -0400

    Create start.py

---
+++ Challenges/7. Leap Year/start.py
@@ -0,0 +1,6 @@
+#Write a program that works out whether if a given year is a leap year.
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
