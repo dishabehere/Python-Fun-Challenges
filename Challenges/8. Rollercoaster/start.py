bf0ccd9e1240e54b801dbee4d9f61e02007de27e
Disha Behere <123135306+dishabehere@users.noreply.github.com>
2024-04-03 14:38:26 -0400

    Create start.py

---
+++ Challenges/8. Rollercoaster/start.py
@@ -0,0 +1,4 @@
+#Write a program that takes user's height as input and if it is greater than 120, allows to ride the rollercoaster
+# <12 "5$", 12-18 "7$", >18 "12$"
+#Ask if they want photos add 3$ to the bill if so.
+#Write your code below this line 👇
